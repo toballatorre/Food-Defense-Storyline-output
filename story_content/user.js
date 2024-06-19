a065d13acbba33bccc4b43beb0f2ea22aab04984
Cristobal Latorre Padilla <clatorre@bcnschool.cl>
2024-06-19 01:09:26 -0400

    first commit

---
+++ story_content/user.js
@@ -0,0 +1,446 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "5YF2Xf6qzek":
+        Script1();
+        break;
+      case "6VCkGQAPvdi":
+        Script2();
+        break;
+      case "6RMI0Qx5uF1":
+        Script3();
+        break;
+      case "6b8VLfUrQN7":
+        Script4();
+        break;
+      case "6ZlGPoDCsLH":
+        Script5();
+        break;
+      case "6VeE6TjK801":
+        Script6();
+        break;
+      case "6a5BpLoGuMu":
+        Script7();
+        break;
+      case "6FWLQ3v5554":
+        Script8();
+        break;
+      case "5ZILkoKIOaA":
+        Script9();
+        break;
+      case "5wMrHzBNFd9":
+        Script10();
+        break;
+      case "6IQxNHznyMn":
+        Script11();
+        break;
+      case "5XlGyEGoYzf":
+        Script12();
+        break;
+      case "5mVr9AcfmAx":
+        Script13();
+        break;
+      case "6bXAK0Kloml":
+        Script14();
+        break;
+      case "6Q8fHPEUld4":
+        Script15();
+        break;
+      case "6cBdQQF1I1m":
+        Script16();
+        break;
+      case "6Uz2HrnemRr":
+        Script17();
+        break;
+      case "6VvK6idOEP8":
+        Script18();
+        break;
+      case "5nw9qegYkgv":
+        Script19();
+        break;
+      case "6ifIAXm2fgP":
+        Script20();
+        break;
+      case "6Uo9QdpMphP":
+        Script21();
+        break;
+      case "5WbVWs4Rjn4":
+        Script22();
+        break;
+  }
+}
+
+function Script1()
+{
+  function add_line() {
+var line = document.createElement("audio");
+var head=document.getElementsByTagName('body')[0];
+line.type = "audio/mp3";
+line.src="";
+line.id="bgSong";
+line.autoplay = true;
+line.loop = true;
+head.appendChild(line);
+}
+
+//this code makes sure that the above code only executes once. It also sets the starting audio volume to 2%
+
+if(document.getElementById('bgSong')==null){
+add_line();
+var audio = document.getElementById('bgSong');
+audio.volume = 0.02;
+audio.src="story_content/external_files/music.mp3";
+audio.load();
+audio.pause();
+}
+}
+
+function Script2()
+{
+  //this script loads the music into the player you created and plays it.
+var audio = document.getElementById('bgSong');
+//audio.src="story_content/external_files/music.mp3";
+//audio.load();
+audio.play();
+}
+
+function Script3()
+{
+  function add_line() {
+var line = document.createElement("audio");
+var head=document.getElementsByTagName('body')[0];
+line.type = "audio/mp3";
+line.src="";
+line.id="bgSong";
+line.autoplay = true;
+line.loop = true;
+head.appendChild(line);
+}
+
+//this code makes sure that the above code only executes once. It also sets the starting audio volume to 2%
+
+if(document.getElementById('bgSong')==null){
+add_line();
+var audio = document.getElementById('bgSong');
+audio.volume = 0.02;
+audio.src="story_content/external_files/music.mp3";
+audio.load();
+audio.pause();
+}
+}
+
+function Script4()
+{
+  var audio = document.getElementById('bgSong');
+if(audio.paused == true){
+audio.play();
+}
+
+}
+
+function Script5()
+{
+  function add_line() {
+var line = document.createElement("audio");
+var head=document.getElementsByTagName('body')[0];
+line.type = "audio/mp3";
+line.src="";
+line.id="bgSong";
+line.autoplay = true;
+line.loop = true;
+head.appendChild(line);
+}
+
+//this code makes sure that the above code only executes once. It also sets the starting audio volume to 2%
+
+if(document.getElementById('bgSong')==null){
+add_line();
+var audio = document.getElementById('bgSong');
+audio.volume = 0.02;
+audio.src="story_content/external_files/music.mp3";
+audio.load();
+audio.pause();
+}
+}
+
+function Script6()
+{
+  var audio = document.getElementById('bgSong');
+if(audio.paused == true){
+audio.play();
+}
+
+}
+
+function Script7()
+{
+  function add_line() {
+var line = document.createElement("audio");
+var head=document.getElementsByTagName('body')[0];
+line.type = "audio/mp3";
+line.src="";
+line.id="bgSong";
+line.autoplay = true;
+line.loop = true;
+head.appendChild(line);
+}
+
+//this code makes sure that the above code only executes once. It also sets the starting audio volume to 2%
+
+if(document.getElementById('bgSong')==null){
+add_line();
+var audio = document.getElementById('bgSong');
+audio.volume = 0.02;
+audio.src="story_content/external_files/music.mp3";
+audio.load();
+audio.pause();
+}
+}
+
+function Script8()
+{
+  var audio = document.getElementById('bgSong');
+if(audio.paused == true){
+audio.play();
+}
+
+}
+
+function Script9()
+{
+  function add_line() {
+var line = document.createElement("audio");
+var head=document.getElementsByTagName('body')[0];
+line.type = "audio/mp3";
+line.src="";
+line.id="bgSong";
+line.autoplay = true;
+line.loop = true;
+head.appendChild(line);
+}
+
+//this code makes sure that the above code only executes once. It also sets the starting audio volume to 2%
+
+if(document.getElementById('bgSong')==null){
+add_line();
+var audio = document.getElementById('bgSong');
+audio.volume = 0.02;
+audio.src="story_content/external_files/music.mp3";
+audio.load();
+audio.pause();
+}
+}
+
+function Script10()
+{
+  var audio = document.getElementById('bgSong');
+if(audio.paused == true){
+audio.play();
+}
+
+}
+
+function Script11()
+{
+  function add_line() {
+var line = document.createElement("audio");
+var head=document.getElementsByTagName('body')[0];
+line.type = "audio/mp3";
+line.src="";
+line.id="bgSong";
+line.autoplay = true;
+line.loop = true;
+head.appendChild(line);
+}
+
+//this code makes sure that the above code only executes once. It also sets the starting audio volume to 2%
+
+if(document.getElementById('bgSong')==null){
+add_line();
+var audio = document.getElementById('bgSong');
+audio.volume = 0.02;
+audio.src="story_content/external_files/music.mp3";
+audio.load();
+audio.pause();
+}
+}
+
+function Script12()
+{
+  var audio = document.getElementById('bgSong');
+if(audio.paused == true){
+audio.play();
+}
+
+}
+
+function Script13()
+{
+  function add_line() {
+var line = document.createElement("audio");
+var head=document.getElementsByTagName('body')[0];
+line.type = "audio/mp3";
+line.src="";
+line.id="bgSong";
+line.autoplay = true;
+line.loop = true;
+head.appendChild(line);
+}
+
+//this code makes sure that the above code only executes once. It also sets the starting audio volume to 2%
+
+if(document.getElementById('bgSong')==null){
+add_line();
+var audio = document.getElementById('bgSong');
+audio.volume = 0.02;
+audio.src="story_content/external_files/music.mp3";
+audio.load();
+audio.pause();
+}
+}
+
+function Script14()
+{
+  var audio = document.getElementById('bgSong');
+if(audio.paused == true){
+audio.play();
+}
+
+}
+
+function Script15()
+{
+  function add_line() {
+var line = document.createElement("audio");
+var head=document.getElementsByTagName('body')[0];
+line.type = "audio/mp3";
+line.src="";
+line.id="bgSong";
+line.autoplay = true;
+line.loop = true;
+head.appendChild(line);
+}
+
+//this code makes sure that the above code only executes once. It also sets the starting audio volume to 2%
+
+if(document.getElementById('bgSong')==null){
+add_line();
+var audio = document.getElementById('bgSong');
+audio.volume = 0.02;
+audio.src="story_content/external_files/music.mp3";
+audio.load();
+audio.pause();
+}
+}
+
+function Script16()
+{
+  var audio = document.getElementById('bgSong');
+if(audio.paused == true){
+audio.play();
+}
+
+}
+
+function Script17()
+{
+  function add_line() {
+var line = document.createElement("audio");
+var head=document.getElementsByTagName('body')[0];
+line.type = "audio/mp3";
+line.src="";
+line.id="bgSong";
+line.autoplay = true;
+line.loop = true;
+head.appendChild(line);
+}
+
+//this code makes sure that the above code only executes once. It also sets the starting audio volume to 2%
+
+if(document.getElementById('bgSong')==null){
+add_line();
+var audio = document.getElementById('bgSong');
+audio.volume = 0.02;
+audio.src="story_content/external_files/music.mp3";
+audio.load();
+audio.pause();
+}
+}
+
+function Script18()
+{
+  var audio = document.getElementById('bgSong');
+if(audio.paused == true){
+audio.play();
+}
+
+}
+
+function Script19()
+{
+  function add_line() {
+var line = document.createElement("audio");
+var head=document.getElementsByTagName('body')[0];
+line.type = "audio/mp3";
+line.src="";
+line.id="bgSong";
+line.autoplay = true;
+line.loop = true;
+head.appendChild(line);
+}
+
+//this code makes sure that the above code only executes once. It also sets the starting audio volume to 2%
+
+if(document.getElementById('bgSong')==null){
+add_line();
+var audio = document.getElementById('bgSong');
+audio.volume = 0.02;
+audio.src="story_content/external_files/music.mp3";
+audio.load();
+audio.pause();
+}
+}
+
+function Script20()
+{
+  var audio = document.getElementById('bgSong');
+if(audio.paused == true){
+audio.play();
+}
+
+}
+
+function Script21()
+{
+  function add_line() {
+var line = document.createElement("audio");
+var head=document.getElementsByTagName('body')[0];
+line.type = "audio/mp3";
+line.src="";
+line.id="bgSong";
+line.autoplay = true;
+line.loop = true;
+head.appendChild(line);
+}
+
+//this code makes sure that the above code only executes once. It also sets the starting audio volume to 2%
+
+if(document.getElementById('bgSong')==null){
+add_line();
+var audio = document.getElementById('bgSong');
+audio.volume = 0.02;
+audio.src="story_content/external_files/music.mp3";
+audio.load();
+audio.pause();
+}
+}
+
+function Script22()
+{
+  //this script loads the music into the player you created and plays it.
+var audio = document.getElementById('bgSong');
+audio.src="story_content/external_files/music.mp3";
+audio.pause();
+}
+
